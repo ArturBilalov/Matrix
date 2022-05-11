58c018bd54fe5fb944866c68f2234ab52eec674d
ArturBilalov <art.buddha8@gmail.com>
2022-05-11 21:59:37 +0300

    initial commit

---
+++ MatrixTrainingApp/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MatrixTrainingApp
+//
+//  Created by Artur Bilalov on 11.05.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var dayTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func calculateButton() {
+        
+    }
+    
+}
+
