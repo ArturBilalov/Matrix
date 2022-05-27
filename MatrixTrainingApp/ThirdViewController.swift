5b5248c389b32f871fd38e0003aa014ab29db68f
ArturBilalov <art.buddha8@gmail.com>
2022-05-27 11:11:52 +0300

    create a thirdViewController with datePicker

---
+++ MatrixTrainingApp/ThirdViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ThirdViewController.swift
+//  MatrixTrainingApp
+//
+//  Created by Artur Bilalov on 27.05.2022.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+    
+    let datePicker = UIDatePicker()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dateTextField.inputView = datePicker
+         datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .wheels
+        let localeID = Locale.preferredLanguages.first
+        datePicker.locale = Locale(identifier: localeID!)
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAction))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolbar.setItems([flexSpace, doneButton], animated: true)
+        
+        dateTextField.inputAccessoryView = toolbar
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureDone))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func doneAction() {
+        getDateFromPicker()
+        view.endEditing(true)
+    }
+    
+    @objc func tapGestureDone() {
+        view.endEditing(true)
+    }
+    
+    func getDateFromPicker() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        dateTextField.text = formatter.string(from: datePicker.date)
+    }
+    
+    
+    
+
+
+}
